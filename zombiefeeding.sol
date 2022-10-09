6e5a92db1146c70d7bc36e72219826242db690ee
yumerov <yumerov.levent@gmail.com>
2022-10-09 17:26:20 +0300

    [Week 0][Lesson 3] Flexible contract using setting vie method + ownable solution

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -19,8 +19,11 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  KittyInterface kittyContract;
+
+  function setKittyContractAddress(address _address) external onlyOwner {
+    kittyContract = KittyInterface(_address);
+  }
 
   function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
@@ -28,7 +31,7 @@ contract ZombieFeeding is ZombieFactory {
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
     if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
-        newDna = newDna - newDna % 100 + 99;
+      newDna = newDna - newDna % 100 + 99;
     }
     _createZombie("NoName", newDna);
   }
