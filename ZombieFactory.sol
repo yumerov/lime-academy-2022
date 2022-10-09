6138df70514cfab2d06f9a474322bde26eddc84b
yumerov <yumerov.levent@gmail.com>
2022-10-09 12:17:07 +0300

    Update ZombieFactory.sol
    [Week 0] CryptoZombie - Lesson 2 - storing zombie - owner relation

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -14,8 +14,13 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
