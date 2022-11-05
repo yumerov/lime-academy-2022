3371b7e2482d15172e18019e8217816b7d3fc571
yumerov <yumerov.levent@gmail.com>
2022-11-05 21:56:09 +0200

    [Week 4] Math overflow security breach

---
+++ security/test/00-arithmetic-overflow-and-underflow.js
@@ -0,0 +1,36 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("TimeLock", function () {
+
+    describe("Attack", function () {
+        it("The target contract should not allow over- and underflow attacks", async () => {
+            const [owner] = await ethers.getSigners();
+
+            const TimeLock = await ethers.getContractFactory("TimeLock");
+            const timeLock = await TimeLock.deploy();
+            await timeLock.deployed();
+
+            const Attack = await ethers.getContractFactory("Attack");
+            const attack = await Attack.deploy(timeLock.address);
+            await attack.deployed();
+
+            await timeLock.deposit({
+                value: ethers.utils.parseEther("1.0")
+            });
+
+            await expect(timeLock.withdraw())
+                .to
+                .be
+                .revertedWith('Lock time not expired');
+
+            await expect(attack.attack({
+                value: ethers.utils.parseEther("1.0")
+            }))
+                .to
+                .be
+                .revertedWith('Lock time not expired');
+        });
+
+    });
+});
